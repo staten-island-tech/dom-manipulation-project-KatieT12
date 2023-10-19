1dd5e4a8e14bd12fa805584c354a788797be1e84
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-10-19 10:11:42 -0400

    10/19/23

--- dom.js
+++ dom.js
@@ -1,17 +1,17 @@
-const form = document.querySelector("#form");
+/* const form = document.querySelector("#form");
 console.log(form);
 const DOMSelectors = {
-    form1: document.querySelector("#form"),
+    form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2s:document.querySelectorAll("h2"),
     // select the text box
     // select all the h2s in one property
 };
-/* console.log(DOMSelectors.h2s); */
-DOMSelectors.form1.addEventListener("submit", function(event){
+/* console.log(DOMSelectors.h2s); 
+DOMSelectors.form.addEventListener("submit", function(event){
 event.preventDefault();
 console.log(DOMSelectors.firstName.value);
 DOMSelectors.h2s.forEach(
     (el) => el.textContent = DOMSelectors.firstName.value
     );
-});
+}); */
