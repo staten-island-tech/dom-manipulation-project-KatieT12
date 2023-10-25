ef3615fed166f23811ea21b9bcb4783ab998c6ee
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-10-25 10:04:55 -0400

    qw3eq23e3

--- 21st.js
+++ 21st.js
@@ -3,31 +3,53 @@ const DOMSelectors = {
     button: document.getElementById("btn"),
     box: document.getElementById("container-box"),
     inputTitle: document.querySelector('inputTitle'),
+    inputAuthor: document.querySelector('inputAuthor'),
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h3s:document.querySelectorAll("h3"),
     h4s:document.querySelectorAll("h4"),
-    card:document.querySelector(".card"),
-    inputImage: document.querySelector('link'),
+    card:document.querySelector("card"),
+    inputImage:document.querySelector('link'),
 
     
 };
 
+/* DOMSelectors.button.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.inputTitle);
+    console.log(DOMSelectors.inputImage.value);
+    console.log(DOMSelectors.inputAuthor.value);
+    DOMSelectors.h3s.forEach(
+        (el) => el.textContent = DOMSelectors.inputTitle.value
+        );
+    DOMSelectors.h4s.forEach(
+        (el) => el.textContent = DOMSelectors.inputAuthor.value
+        );
+    DOMSelectors.h4s.forEach(
+        (el) => el.textContent = DOMSelectors.inputImage.value
+        );
+        DOMSelectors.inputTitle.value = " "; 
+        DOMSelectors.inputAuthor.value = " "; 
+        DOMSelectors.inputImage.value = " "; 
+    });  */
+///
+
 function addCard(){
-    document.querySelector(".card").insertAdjacentElement(
-       ("afterend", 
+    document.querySelector("card").insertAdjacentHTML(
+       ("beforeend", 
         `<div class = "card">
-        <h3 id ="title">${inputTitle}</h3>
+        <h3>${inputTitle}</h3>
+        <h4>${inputImage}</h4>
         <img src=${inputImage} alt = "book cover"> </img>
     </div>`
     ))}
 
+
     DOMSelectors.button.addEventListener("click", function(event){
-        DOMSelectors.inputTitle.value = " "; 
-        DOMSelectors.inputImage.value = " "; 
-        event.preventDefault();
         addCard();
-    });
+        form.clear();
+        event.preventDefault();
+    }); 
 
 {/*  DOMSelectors.button.addEventListener("click", function(event){
     let input = DOMSelectors.input.value;
