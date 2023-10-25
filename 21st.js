ee3337e161ec62e319a26f49ffac72a4b5ba2d08
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2023-10-24 20:10:14 -0400

    bruh

--- 21st.js
+++ 21st.js
@@ -2,27 +2,45 @@
 const DOMSelectors = {
     button: document.getElementById("btn"),
     box: document.getElementById("container-box"),
-    input: document.querySelector('input'),
+    inputTitle: document.querySelector('inputTitle'),
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h3s:document.querySelectorAll("h3"),
     h4s:document.querySelectorAll("h4"),
+    card:document.querySelector(".card"),
+    inputImage: document.querySelector('link'),
+
     
 };
-/* const cat = "meow";
-DOMSelectors.box.insertAdjacentHTML("afterend",
-`<h>We are a ${cat}</h1>`); */
 
-DOMSelectors.button.addEventListener("click", function(event){
+function addCard(){
+    document.querySelector(".card").insertAdjacentElement(
+       ("afterend", 
+        `<div class = "card">
+        <h3 id ="title">${inputTitle}</h3>
+        <img src=${inputImage} alt = "book cover"> </img>
+    </div>`
+    ))}
+
+    DOMSelectors.button.addEventListener("click", function(event){
+        DOMSelectors.inputTitle.value = " "; 
+        DOMSelectors.inputImage.value = " "; 
+        event.preventDefault();
+        addCard();
+    });
+
+{/*  DOMSelectors.button.addEventListener("click", function(event){
     let input = DOMSelectors.input.value;
- /*    DOMSelectors.box.insertAdjacentHTML("afterend", 
+    DOMSelectors.box.insertAdjacentHTML("afterend", 
+    <div class = "card">
+        <h3 id ="title">${input}</h3>
+        <img src=""
+    </div>
     `<p> ${input} </p>`);
-    DOMSelectors.input.value = ""; */
+    DOMSelectors.input.value = ""; 
 event.preventDefault();
 console.log(DOMSelectors.input.value);
 DOMSelectors.h3s.forEach(
     (el) => el.textContent = DOMSelectors.input.value
     );
-}); 
-
-////
+}); */}
